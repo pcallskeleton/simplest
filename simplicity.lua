f002a637a979a60ad10647f321430a8634900e7e
pcallskeleton <141058672+pcallskeleton@users.noreply.github.com>
2024-12-25 15:27:08 -0800

    Update simplicity.lua

--- simplicity.lua
+++ simplicity.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/d8bf54daa5b358826ce74cab275f9135.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/4f5c7bbe546251d81e9d3554b109008f.lua"))()

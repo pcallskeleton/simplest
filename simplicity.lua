02f4a7775b9b11f3294451fda6e23ee3e6f274f8
pcallskeleton <141058672+pcallskeleton@users.noreply.github.com>
2024-12-29 19:28:28 -0800

    Update simplicity.lua

--- simplicity.lua
+++ simplicity.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/4f5c7bbe546251d81e9d3554b109008f.lua"))()
+game.Players.LocalPlayer:Kick('Please Join Aussie Studios: discord.gg/ymguJ6kQNK\nThank you! (New script is in their)')
